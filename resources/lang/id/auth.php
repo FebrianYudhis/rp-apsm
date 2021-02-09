eb8bc946ea5c70d5db5988467c8dc77bfab7ad44
FebrianYudhis <killersvteam@gmail.com>
2021-02-09 18:56:26 +0700

    Mengganti Bahasa Dan Memperbaiki Beberapa Tampilan

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Baris-baris bahasa untuk autentifikasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan selama proses autentifikasi untuk beberapa
+    | pesan yang perlu kita tampilkan ke pengguna. Anda bebas untuk memodifikasi
+    | baris bahasa sesuai dengan keperluan aplikasi anda.
+    |
+    */
+
+    'failed'   => 'Email atau password salah',
+    'password' => 'Password yang dimasukkan salah',
+    'throttle' => 'Terlalu banyak upaya masuk. Silahkan coba lagi dalam :seconds detik.',
+];
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, "showLoginForm"]);
 
 Auth::routes();
 
