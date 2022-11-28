2a6651c2a9c6a6a6cb4c167fbf623b410e9d0dff
FebrianYudhis <killersvteam@gmail.com>
2022-11-28 09:14:10 +0700

    Memperbaiki Halaman Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,78 +1,55 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@push('css')
+<style>
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+    }
+</style>
+@endpush
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email"
-                                class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                {{-- @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif --}}
-                            </div>
-                        </div>
-                    </form>
+<div class="splash-container">
+    <div class="card">
+        <div class="card-header text-center">
+            <a href="{{ url('/') }}"><img class="logo-img" src="{{ asset('gambar/icon/Logo.png') }}" alt="logo"
+                    style="width: 100%"></a>
+            <span class="splash-description">Masukkan
+                Informasi
+                Akun Anda</span>
+        </div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group">
+                    <input class="form-control form-control-lg" id="email" type="email" placeholder="Email"
+                        name="email">
                 </div>
-            </div>
+                <div class="form-group">
+                    <input class="form-control form-control-lg" id="password" type="password" placeholder="Password"
+                        name="password">
+                </div>
+                <div class="form-group">
+                    <label class="custom-control custom-checkbox">
+                        <input class="custom-control-input" type="checkbox" name="remember" id="remember"><span
+                            class="custom-control-label">Ingat Saya</span>
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary btn-lg btn-block">Masuk</button>
+            </form>
+        </div>
+        <div class="card-footer bg-white p-0">
+            <a href="{{ url('/register') }}" class="btn btn-success w-100">Buat Akun</a>
         </div>
     </div>
 </div>
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>{{ $judul }}</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('fonts/circular/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/concept.css') }}">
+    <link rel="stylesheet" href="{{ asset('fonts/fontawesome/css/fontawesome-all.css') }}">
+    @stack('css')
+</head>
+
+<body>
+    @yield('content')
+    <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    @stack('js')
+</body>
+
+</html>
