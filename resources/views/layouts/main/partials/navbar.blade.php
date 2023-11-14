80023f01e562f3e6c4fe3de4e695675f51ce3482
FebrianYudhis <killersvteam@gmail.com>
2023-11-14 09:04:15 +0700

    Versi 1.4.3

--- resources/views/layouts/main/partials/navbar.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="dashboard-header">
-    <nav class="navbar navbar-expand-lg bg-white fixed-top">
-        <a class="navbar-brand" href="{{ url('/app') }}">APSM Tahun {{ auth()->user()->tahun }}</a>
-    </nav>
-</div>
